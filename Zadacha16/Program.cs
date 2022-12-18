fae5296d68edd05ebaa21c3799225a3227990a82
Kirill Karavaev <coffee.2016@yandex.ru>
2022-12-18 11:26:33 +0300

    Добавлена еще одна задача из самостоятельной работы

---
+++ Zadacha16/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число");
+int FirstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число");
+int SecondNum = Convert.ToInt32(Console.ReadLine());
+
+if ((FirstNum == SecondNum*SecondNum) | (SecondNum == FirstNum*FirstNum))
+    Console.WriteLine("Является");
+else 
+    Console.WriteLine("Не является");
+    
