bddd48ffe6d32bb502b2db9f61655d8bcbae5a77
Kirill Karavaev <coffee.2016@yandex.ru>
2022-12-18 20:16:46 +0300

    Добавлена задача 15 из дз

---
+++ Zadacha15dz/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if ((Number % 6 == 0) | (Number % 7 == 0))
+{
+    Console.WriteLine("Да, сегодня выходной");
+}
+else 
+{
+    Console.WriteLine("Эх, трудовые будни");
+}
