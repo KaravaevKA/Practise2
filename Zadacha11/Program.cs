44e48266079bb7b7aacef849d3fee50653f2d2d7
Kirill Karavaev <coffee.2016@yandex.ru>
2022-12-18 11:15:55 +0300

    задачи из самостоятельной работы на семинаре

---
+++ Zadacha11/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = new Random().Next(100,1000);
+
+int firstNum = number/100;
+int lastNum = number%10;
+
+Console.WriteLine(number);
+Console.WriteLine(firstNum*10+lastNum);
---
+++ Zadacha12/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write($"Введите первое число: ");
+int numFirst = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите второе число: ");
+int numSecond = Convert.ToInt32(Console.ReadLine());
+
+int div = numFirst % numSecond;
+
+if (div == 0) {
+    Console.WriteLine($"Кратно");
+}
+else {
+    Console.WriteLine($"Не кратно. Остаток {div}");
+}
