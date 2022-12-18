0c297d4743f72a3efb10df7be717d40f8c856efc
Kirill Karavaev <coffee.2016@yandex.ru>
2022-12-18 20:51:23 +0300

    Добавлена задача 13, но она не работает. Нужна помощь

---
+++ Zadacha13dz/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int a = Number % 10;
+if (a % 10 < 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+else
+
+{
+    Console.WriteLine(a);
+}
